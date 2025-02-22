5758d4c624dfb692edf0b5be8aa776ff0a6f31fe
Paul Baur <paul.baur@gmail.com>
2025-02-21 20:17:39 -0600

    Troubleshooting OpenAPI/Swagger

--- src/main/java/me/paulbaur/taskman/config/OpenApiConfig.java
+++ src/main/java/me/paulbaur/taskman/config/OpenApiConfig.java
@@ -5,11 +5,13 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
 
+    @Bean
     public OpenAPI taskManagerOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
--- src/main/java/me/paulbaur/taskman/controller/TaskController.java
+++ src/main/java/me/paulbaur/taskman/controller/TaskController.java
@@ -7,7 +7,7 @@ import me.paulbaur.taskman.service.TaskService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.parameters.Parameter;
+//import io.swagger.v3.oas.annotations.parameters.Parameter;
 
 import me.paulbaur.taskman.exception.ResourceNotFoundException;
 import org.springframework.http.ResponseEntity;
