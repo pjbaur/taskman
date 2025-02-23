d6a9bacaecaff1c2f6d378fb4e288e1995ae9bd4
Paul Baur <paul.baur@gmail.com>
2025-02-23 00:41:13 -0600

    Clean up imports in OpenApiConfig

--- src/main/java/me/paulbaur/taskman/config/OpenApiConfig.java
+++ src/main/java/me/paulbaur/taskman/config/OpenApiConfig.java
@@ -1,30 +1,27 @@
 package me.paulbaur.taskman.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(info = @io.swagger.v3.oas.annotations.info.Info(
-        title = "Simple API Demo",
+        title = "Task Manager API",
         version = "1.0",
-        description = "Demo API Documentation"))
+        description = "Backend REST API for a Task Manager application"))
 public class OpenApiConfig {
 
     @Bean
     public OpenAPI taskManagerOpenAPI() {
         return new OpenAPI()
                 .info(new io.swagger.v3.oas.models.info.Info()
-                        .title("Custom API Title")
+                        .title("Task Manager API")
                         .version("1.0.0")
                         .contact(new Contact()
-                                .name("Support")
-                                .email("support@example.com"))
-                        .description("API Documentation"));
+                                .name("Paul Baur")
+                                .email("paul.baur@gmail.com"))
+                        .description("Task Manager API Documentation"));
     }
 }
